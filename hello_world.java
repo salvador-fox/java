1d74856c539feb02fb7252f04202bb322a7217aa
salvador <salvador.martin@fii-na.com>
2021-03-05 14:52:13 -0600

    cambio 6

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,20 @@ public class hello_world {
 
        System.out.println("Hello World");
 
+System.out.println("Hola usuario bienvenido");
+        System.out.println("A continuación se muestra una cuenta regresiva");
+
+        for (int i = 10; i >= 1;  i--)
+        {
+            System.out.println(i);//Se muestra cada valor de i (de 10 al 1)
+        }
+
+        System.out.println("Ahora, otra cuenta regresiva");
+
+        for (int i = 10; i >= 1;  i--)
+        {
+            System.out.print(i);//Se muestra cada valor de i (de 1 a 10)
+        }
 }
 
 
